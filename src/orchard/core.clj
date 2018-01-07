d3e5ded6e7c49ad78d58e3b14bccb4f7e06955e2
Bozhidar Batsov <bozhidar@batsov.com>
2018-01-07 23:22:13 +0200

    Blank the core namespace

--- src/orchard/core.clj
+++ src/orchard/core.clj
@@ -1,6 +1,1 @@
 (ns orchard.core)
-
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
--- test/orchard/core_test.clj
+++ test/orchard/core_test.clj
@@ -1,7 +1,3 @@
 (ns orchard.core-test
   (:require [clojure.test :refer :all]
             [orchard.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
