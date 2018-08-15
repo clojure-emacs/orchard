88d6cf69998ddd5934fbb30c714ffe1370860ec1
Bozhidar Batsov <bozhidar@batsov.com>
2018-08-15 12:05:18 +0200

    Add SCM info to the project

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/clojure-emacs/orchard"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git" :url "https://github.com/clojure-emacs/orchard"}
   :dependencies [;; We can't update dynapath to 1.0 as it removes the functionality we need from it
                  ;; We have to eventually apply the fix outlined here
                  ;; https://github.com/tobias/dynapath#note-on-urlclassloader
