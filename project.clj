ff48aca7316437ab63ed9cc0df07257f502ed511
Bozhidar Batsov <bozhidar@batsov.com>
2018-10-10 22:50:22 +0300

    Bump the development version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/orchard "0.3.1"
+(defproject cider/orchard "0.3.2-SNAPSHOT"
   :description "A fertile ground for Clojure tooling"
   :url "https://github.com/clojure-emacs/orchard"
   :license {:name "Eclipse Public License"
