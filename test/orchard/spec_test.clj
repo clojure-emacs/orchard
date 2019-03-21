3caca641ccbb1c81de29c786ffb0723aecdf3e6c
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2019-03-21 08:51:48 +0200

    Add namespace alias support for `spec-list` and `spec-form` (#42)

--- test/orchard/spec_test.clj
+++ test/orchard/spec_test.clj
@@ -20,3 +20,35 @@
                     (clojure.core/fn [%] (clojure.core/< (:start %) (:end %))))
              :ret (clojure.core/fn [%] (clojure.core/> (:start %) (:end %)))
              :fn nil)))))
+
+(def spec-available? (or (resolve (symbol "clojure.spec.alpha" "get-spec"))
+                         (resolve (symbol "clojure.spec" "get-spec"))))
+
+(when spec-available?
+  (deftest spec-is-found-by-ns-alias
+    (testing "current ns keyword"
+      (testing "spec-list finds current ns keyword"
+        (eval '(clojure.spec.alpha/def ::foo string?))
+        (let [specs (into #{}
+                          (spec/spec-list "" "orchard.spec-test"))]
+          (is (specs "::foo") "Spec is found with current ns")
+          (is (specs ":orchard.spec-test/foo") "Spec is found with fully qualified name")))
+
+      (testing "spec-form finds current ns keyword"
+        (let [spec1 (spec/spec-form "::foo" "orchard.spec-test")
+              spec2 (spec/spec-form ":orchard.spec-test/foo" "orchard.spec-test")]
+          (is (= "clojure.core/string?" spec1 spec2) "Both return the same correct spec"))))
+
+    (testing "ns aliased keyword"
+      (eval '(clojure.spec.alpha/def :orchard.spec/test-dummy boolean?))
+      (testing "spec-list finds keyword in aliased namespace"
+
+        (let [specs (into #{}
+                          (spec/spec-list "" "orchard.spec-test"))]
+          (is (specs "::spec/test-dummy") "Spec is found with ns-aliased keyword")
+          (is (specs ":orchard.spec/test-dummy") "Spec is found with fully qualified name")))
+
+      (testing "spec-form finds keyword in aliased namespace"
+        (let [spec1 (spec/spec-form "::spec/test-dummy" "orchard.spec-test")
+              spec2 (spec/spec-form ":orchard.spec/test-dummy" "orchard.spec-test")]
+          (is (= "clojure.core/boolean?" spec1 spec2) "Both return the same correct spec"))))))
