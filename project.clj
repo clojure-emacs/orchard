052c155985a30f4761ef745b269c873637db26d7
Bozhidar Batsov <bozhidar@batsov.com>
2019-06-06 12:08:03 +0300

    Bump to 0.5.0-beta5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cider/orchard "0.5.0-beta4"
+(defproject cider/orchard "0.5.0-beta5"
   :description "A fertile ground for Clojure tooling"
   :url "https://github.com/clojure-emacs/orchard"
   :license {:name "Eclipse Public License"
