b5e602b8a957b6767ddf603462fb4e0cea73e78d
Andrea Richiardi <a.richiardi.work@gmail.com>
2019-07-26 12:10:18 -0700

    Delete transform-value
    The function will go to cider-nrepl, where it belongs as it seems to be mainly
    related to presentation only.

--- src/orchard/misc.clj
+++ src/orchard/misc.clj
@@ -102,42 +102,6 @@
 (def java-api-version
   (parse-java-version (System/getProperty "java.version")))
 
-(defmulti transform-value "Transform a value for output" type)
-
-(defmethod transform-value :default [v] (str v))
-
-(defmethod transform-value Number [v] v)
-
-(defmethod transform-value nil [v] nil)
-
-(defmethod transform-value java.io.File
-  [v]
-  (.getAbsolutePath ^java.io.File v))
-
-(defmethod transform-value clojure.lang.Sequential
-  [v]
-  (list* (map transform-value v)))
-
-(defmethod transform-value clojure.lang.Symbol
-  [v]
-  (let [[the-ns the-name] [(namespace v) (name v)]]
-    (if the-ns
-      (str the-ns "/" the-name)
-      the-name)))
-
-(defmethod transform-value clojure.lang.Keyword
-  [v]
-  (transform-value (.sym ^clojure.lang.Keyword v)))
-
-(defmethod transform-value clojure.lang.Associative
-  [m]
-  (->> (for [[k v] m] ; bencode keys must be strings
-         [(str (transform-value k)) (transform-value v)])
-       (into {})))
-
-;; handles vectors
-(prefer-method transform-value clojure.lang.Sequential clojure.lang.Associative)
-
 ;; TODO move back to analysis.cljs
 (defn add-ns-macros
   "Append $macros to the input symbol"
