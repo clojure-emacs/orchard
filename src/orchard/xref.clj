a5301ca98a75ab26b1893298eeda8b2c4400895d
Lukas Domagala <lukas.domagala@brandung.de>
2022-01-02 22:47:28 +0100

    fn-deps for lambdas

--- src/orchard/xref.clj
+++ src/orchard/xref.clj
@@ -3,7 +3,47 @@
   references."
   {:added "0.5"}
   (:require
+   [clojure.tools.reader :as r]
+   [clojure.tools.reader.reader-types :as rts]
+   [clojure.repl :as repl]
    [orchard.query :as q]))
+
+;;;;;;; taken from sayid, needs to be cleaned up
+(defn- mk-dummy-whitespace
+  [lines cols]
+  (apply str
+         (concat (repeat lines "\n")
+                 (repeat cols " "))))
+
+(defn- mk-positionalble-src-logging-push-back-rdr
+  [s file line col]
+  (rts/source-logging-push-back-reader (str (mk-dummy-whitespace (dec line) ;;this seem unfortunate
+                                                                 (dec col))
+                                            s)
+                                       (+ (count s) line col 1)
+                                       file))
+
+(defn- hunt-down-source
+  [fn-sym]
+  (let [{:keys [source file line column]} (-> fn-sym
+                                              resolve
+                                              meta)]
+    ;; (println file)
+    (or source
+        (and file (r/read (mk-positionalble-src-logging-push-back-rdr
+                           (or
+                            (clojure.repl/source-fn fn-sym)
+                            (->> file
+                                 slurp
+                                 clojure.string/split-lines
+                                 (drop (dec line))
+                                 (clojure.string/join "\n"))
+                            "nil")
+                           file
+                           line
+                           column))))))
+
+;;;;;;; taken from sayid, needs to be cleaned up
 
 (defn- as-val
   "Convert `thing` to a function value."
@@ -13,20 +53,76 @@
     (symbol? thing) (var-get (find-var thing))
     (fn? thing) thing))
 
-(defn fn-deps
-  "Returns a set with all the functions invoked by `val`.
-  `val` can be a function value, a var or a symbol."
-  {:added "0.5"}
+(defn- f->sym [f]
+  (-> f .getClass .getName repl/demunge symbol))
+
+(defn- fn-source [f]
+  (hunt-down-source (f->sym f)))
+
+(defn- fn-name [f]
+  (-> f .getName repl/demunge symbol))
+
+(defonce classbytes (atom {}))
+
+(defn- recompile [ns-sym form]
+  (push-thread-bindings
+   {clojure.lang.Compiler/LOADER
+    (proxy [clojure.lang.DynamicClassLoader] [@clojure.lang.Compiler/LOADER]
+      (defineClass
+        ([name bytes src]
+         (swap! classbytes assoc name bytes)
+         (proxy-super defineClass name bytes src))))})
+  (try
+    (let [line @clojure.lang.Compiler/LINE
+          column @clojure.lang.Compiler/COLUMN
+          line (if-let [line (:line (meta form))]
+                 line
+                 line)
+          column (if-let [column (:column (meta form))]
+                   column
+                   column)]
+      (push-thread-bindings {clojure.lang.Compiler/LINE line
+                             clojure.lang.Compiler/COLUMN column})
+      (try
+        (let [form (macroexpand form)]
+          (binding [*ns* (create-ns ns-sym)]
+            (clojure.lang.Compiler/analyze
+             clojure.lang.Compiler$C/EVAL
+             (nth form 2))))
+        (finally
+          (pop-thread-bindings))))
+    (finally
+      (pop-thread-bindings))))
+
+
+
+(defn- fn-deps-class
   [val]
-  (let [val (as-val val)]
-    (set (some->> val class .getDeclaredFields
+  (let [v (if (class? val)
+            val
+            (eval val))]
+    (set (some->> v .getDeclaredFields
                   (keep (fn [^java.lang.reflect.Field f]
                           (or (and (identical? clojure.lang.Var (.getType f))
                                    (java.lang.reflect.Modifier/isPublic (.getModifiers f))
                                    (java.lang.reflect.Modifier/isStatic (.getModifiers f))
                                    (-> f .getName (.startsWith "const__"))
-                                   (.get f val))
+                                   (.get f (fn-name v)))
                               nil)))))))
+
+(defn fn-deps
+  "Returns a set with all the functions invoked by `val`.
+  `val` can be a function value, a var or a symbol."
+  {:added "0.5"}
+  [f]
+  (reset! classbytes {})
+  (let [f (as-val f)]
+    (when-let [source (and (fn? f) (fn-source f))]
+      (let [sym (f->sym f)
+            b (do (recompile (-> sym namespace symbol) source) @classbytes)
+            class-names (map first b)
+            deps (set (mapcat #(-> % symbol fn-deps-class) class-names))]
+        deps))))
 
 (defn- fn->sym
   "Convert a function value `f` to symbol."
@@ -50,3 +146,16 @@
         all-vars (q/vars {:ns-query {:project? true} :private? true})
         deps-map (zipmap all-vars (map fn-deps all-vars))]
     (map first (filter (fn [[_k v]] (contains? v var)) deps-map))))
+
+(defn- dummy-fn [_x]
+  (map #(* % 2) (filter even? (range 1 10))))
+
+(comment
+
+  (fn-deps #'user/jdk8?)
+  (fn-deps #'orchard.meta/ns-file)
+  (def vars (q/vars {:ns-query {:project? true} :private? true}))
+  (map fn-deps vars)
+  (drop 100 vars)
+  (when-let [x (fn? (as-val user/jdk8?))]
+    "some"))
--- test/orchard/xref_test.clj
+++ test/orchard/xref_test.clj
@@ -3,22 +3,25 @@
    [clojure.test :refer [deftest is testing]]
    [orchard.xref :as xref]))
 
+(defn- times [a b]
+  (* a b))
+
 (defn- dummy-fn [_x]
-  (map #(* % 2) (filter even? (range 1 10))))
+  (map #(times % 2) (filter even? (range 1 10))))
 
 (deftest fn-deps-test
   (testing "with a fn value"
     (is (= (xref/fn-deps dummy-fn)
            #{#'clojure.core/map #'clojure.core/filter
-             #'clojure.core/even? #'clojure.core/range})))
+             #'clojure.core/even? #'clojure.core/range #'orchard.xref-test/times})))
   (testing "with a var"
     (is (= (xref/fn-deps #'dummy-fn)
            #{#'clojure.core/map #'clojure.core/filter
-             #'clojure.core/even? #'clojure.core/range})))
+             #'clojure.core/even? #'clojure.core/range #'orchard.xref-test/times})))
   (testing "with a symbol"
     (is (= (xref/fn-deps 'orchard.xref-test/dummy-fn)
            #{#'clojure.core/map #'clojure.core/filter
-             #'clojure.core/even? #'clojure.core/range}))))
+             #'clojure.core/even? #'clojure.core/range #'orchard.xref-test/times}))))
 
 ;; The mere presence of this var can reproduce a certain issue. See:
 ;; https://github.com/clojure-emacs/orchard/issues/135#issuecomment-939731698
@@ -38,4 +41,7 @@
     (is (contains? (into #{} (xref/fn-refs #'map)) #'orchard.xref-test/dummy-fn)))
   (testing "with a symbol"
     (is (= (xref/fn-refs 'orchard.xref-test/dummy-fn) '()))
-    (is (contains? (into #{} (xref/fn-refs #'map)) #'orchard.xref-test/dummy-fn))))
+    (is (contains? (into #{} (xref/fn-refs #'map)) #'orchard.xref-test/dummy-fn)))
+  (testing "with a lambda"
+    (is (= (xref/fn-refs dummy-fn) '()))
+    (is (contains? (into #{} (xref/fn-refs #'times)) #'orchard.xref-test/dummy-fn))))
