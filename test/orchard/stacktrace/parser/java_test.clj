b82e4d4b1b99217750b5e06d87c6f982a9ea9290
Roman Scherer <roman@burningswell.com>
2022-09-25 13:27:14 +0200

    Test Java parser on short input

--- test/orchard/stacktrace/parser/java_test.clj
+++ test/orchard/stacktrace/parser/java_test.clj
@@ -55,6 +55,32 @@
     (testing "throwable trace"
       (is (every? test/stacktrace-element? trace)))))
 
+(deftest parse-stacktrace-short-test
+  (let [{:keys [cause data trace product via]} (test/parse-fixture :short.java)]
+    (testing "product"
+      (is (= :java product)))
+    (testing "throwable cause"
+      (is (= "BOOM-1" cause)))
+    (testing "throwable data"
+      (is (= {:boom "1"} data)))
+    (testing "throwable via"
+      (is (= 1 (count via)))
+      (testing "stacktrace first cause"
+        (let [{:keys [at data message type]} (nth via 0)]
+          (is (test/stacktrace-element? at))
+          (is (= {:boom "1"} data))
+          (is (= "BOOM-1" message))
+          (is (= 'clojure.lang.ExceptionInfo type)))))
+    (testing "throwable trace"
+      (is (= 1 (count trace)))
+      (is (every? test/stacktrace-element? trace))
+      (testing "stacktrace first frame"
+        (let [[class method file line] (first trace)]
+          (is (= 'orchard.stacktrace.parser.throwable_test$eval16655 class))
+          (is (= 'invokeStatic method))
+          (is (= "form-init17983781294375166615.clj" file))
+          (is (= 74 line)))))))
+
 (deftest parse-stacktrace-garbage-test
   (let [expected (test/read-fixture :boom.java)]
     (testing "parsing a stacktrace with garbage at the end should succeed"
