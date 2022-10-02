a0cddac42df164edef3226db8f804f645073b15d
Roman Scherer <roman@burningswell.com>
2022-10-02 16:16:38 +0200

    Improve parser tests

--- test/orchard/stacktrace/parser/test.clj
+++ test/orchard/stacktrace/parser/test.clj
@@ -4,15 +4,39 @@
    [clojure.walk :as walk])
   (:import java.util.regex.Pattern))
 
-(defn fixture [resource]
+(def exceptions
+  "The fixture exceptions."
+  #{:boom :divide-by-zero :short})
+
+(def formats
+  "The fixture formats."
+  #{:aviso :clojure :java :pst})
+
+(def fixtures
+  "The fixture names as keywords."
+  (for [exception exceptions, format formats]
+    (keyword (str (name exception) "." (name format)))))
+
+(defn fixture
+  "Return the fixture path for the parser `resource`."
+  [resource]
   (str (io/file "orchard" "stacktrace" "parser" (str (name resource) ".txt"))))
 
-(defn read-fixture [name]
+(defn read-fixture
+  "Read the fixture `name`."
+  [name]
   (some-> name fixture io/resource slurp))
 
-(defn stacktrace-element? [element]
-  (symbol? (first element)))
+(defn stacktrace-element?
+  "Return true if `element` is a stacktrace element, otherwise false."
+  [[class method file :as element]]
+  (and (symbol? class)
+       (symbol? method)
+       (string? file)))
 
-(defn stringify-regexp [x]
+(defn stringify-regexp
+  "Post-walk `x` and replace all instances of `java.util.regex.Pattern`
+  in it by applying `clojure.core/str` on them."
+  [x]
   (cond->> (walk/postwalk #(if (instance? Pattern %) (str %) %) x)
     (map? x) (into {})))
--- test/orchard/stacktrace/parser_test.clj
+++ test/orchard/stacktrace/parser_test.clj
@@ -4,9 +4,22 @@
    [orchard.stacktrace.parser :as parser]
    [orchard.stacktrace.parser.test :as test]))
 
+(deftest parse-test
+  (doseq [fixture test/fixtures]
+    (testing (format "parse fixture %s" fixture)
+      (let [{:keys [cause error trace type] :as x} (parser/parse (test/read-fixture fixture))]
+        (testing "should succeed"
+          (is (nil? error)))
+        (testing "should parse the cause"
+          (is (string? cause)))
+        (testing "should parse the trace"
+          (is (every? test/stacktrace-element? trace)))))))
+
 (deftest parse-input-transformation-test
-  (let [text (test/read-fixture :boom.java)]
-    (testing "transformation of pr-str 1 level deep"
-      (is (= (parser/parse text) (parser/parse (pr-str text)))))
-    (testing "transformation of pr-str 2 levels deep"
-      (is (= (parser/parse text) (parser/parse (pr-str (pr-str text))))))))
+  (doseq [fixture test/fixtures]
+    (testing (format "parse fixture %s" fixture)
+      (let [text (test/read-fixture fixture)]
+        (testing "with input pr-str 1 level deep"
+          (is (= (parser/parse text) (parser/parse (pr-str text)))))
+        (testing "with input pr-str 2 levels deep"
+          (is (= (parser/parse text) (parser/parse (pr-str (pr-str text))))))))))
