43ff4da12743f1af0c2bc4d06f9c0e433cd89843
vemv <vemv@users.noreply.github.com>
2023-11-15 10:22:51 +0100

    `orchard.inspect`: introduce `tap-indexed` function

--- project.clj
+++ project.clj
@@ -84,7 +84,7 @@
                                          `add-cognitest)]}
 
              ;; Development tools
-             :dev {:plugins [[cider/cider-nrepl "0.43.1"]
+             :dev {:plugins [[cider/cider-nrepl "0.43.3"]
                              [refactor-nrepl "3.9.0"]]
                    :dependencies [[nrepl/nrepl "1.0.0"]
                                   [org.clojure/tools.namespace "1.4.4"]]
