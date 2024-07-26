ba4cd7ce4f41ddd1d07fd99fb54742be1d8224fa
Oleksandr Yakushev <alex@bytopia.org>
2024-07-26 15:50:37 +0300

    [ci] Use Noble docker images

--- test/orchard/sanity_test.clj
+++ test/orchard/sanity_test.clj
@@ -1,7 +1,7 @@
 (ns orchard.sanity-test
   (:require [clojure.test :refer [deftest is]]))
 
-(deftest version-sanity-check
+(deftest clojure-version-sanity-check
   (is (let [v (System/getenv "CLOJURE_VERSION")]
         (println "Running on Clojure" (clojure-version) ", expected:" v)
         (or (nil? v) (.startsWith ^String (clojure-version) v)))))
