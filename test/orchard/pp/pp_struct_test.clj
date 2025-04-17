0c06352b525923725a6aee18bab4df05b5fa9eea
Roman Scherer <roman@burningswell.com>
2025-04-17 11:28:19 +0200

    Remove most of the copied pp tests

--- test/orchard/pp/pp_struct_test.clj
+++
@@ -1,6 +0,0 @@
-(ns orchard.pp.pp-struct-test
-  (:require [clojure.test :refer [deftest]]
-            [orchard.pp.test :refer [$]]))
-
-(deftest pprint-struct
-  ($ (struct (create-struct :q/a :q/b :q/c) 1 2 3)))
